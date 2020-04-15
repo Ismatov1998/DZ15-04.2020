319dd0cfbc0477f59e7af65e692806e8fcc65bf9
Азиз Исматов <yourGitHub@example.co>
2020-04-16 02:32:19 +0500

    Добавил еще 2 функции для удаления и обновления

--- Program.cs
+++ Program.cs
@@ -92,7 +92,67 @@ namespace Fg
            // var result = command.ExecuteNonQuery();
             reader.Close();
             }
+            if(n==5)
+            {
+             Console.WriteLine("введите ID");
+            int a1=Convert.ToInt32(Console.ReadLine());  
+
+             string insertSqlCommand = string.Format($"delete Person where id={a1}");
+            SqlCommand command = new SqlCommand(insertSqlCommand, con);
+             var result = command.ExecuteNonQuery();
+
+            string commandText = "Select * from Person";
+            command = new SqlCommand(commandText, con);
+            
+            SqlDataReader reader = command.ExecuteReader();
+            Console.WriteLine($"Таблица после удаления {a1} го строка");
+            while (reader.Read())
+            {
+                System.Console.WriteLine($"ID: {reader.GetValue("id")},FirstName: {reader.GetValue("FirstName")},LastName: {reader.GetValue("LastName")},MiddleName: {reader.GetValue("MiddleName")},BirthDate: {reader.GetValue("BirthDate")}");
+            }
+            
+            reader.Close();
+
+                
+             
+             
            
+            }
+            if(n==4)
+            {
+                Console.WriteLine("Введите FirstName");
+                Console.WriteLine("Введите LastName");
+                Console.WriteLine("Введите MiddleName");
+                Console.WriteLine("Введите DateTime ");
+                Console.WriteLine("Введите ID стороку который хотите обновлять");
+                string s1=(Console.ReadLine());
+                string s2=(Console.ReadLine());
+                string s3=(Console.ReadLine());
+                string s4=(Console.ReadLine());
+                int n2=Convert.ToInt32(Console.ReadLine());
+
+                string insertSqlCommand = $"Update Person set FirstName='{s1}',LastName='{s2}',MiddleName='{s3}',BirthDate='{s4}' where id={n2}";
+                SqlCommand command = new SqlCommand(insertSqlCommand, con);
+                var result = command.ExecuteNonQuery();
+
+                string commandText = "Select * from Person";
+                command = new SqlCommand(commandText, con); 
+
+                SqlDataReader reader = command.ExecuteReader();
+                Console.WriteLine($"Таблица после обновления");
+                 while (reader.Read())
+                 {
+                     
+                    System.Console.WriteLine($"ID: {reader.GetValue("id")},FirstName: {reader.GetValue("FirstName")},LastName: {reader.GetValue("LastName")},MiddleName: {reader.GetValue("MiddleName")},BirthDate: {reader.GetValue("BirthDate")}");
+                 }
+                   
+                
+                   reader.Close();
+
+                
+
+
+            }
         
           
            
